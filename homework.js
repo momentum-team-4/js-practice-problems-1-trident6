12b4d3f1d72c759748ce44c338d5230de8c09680
Philip Johnson <philip.johnson.it@gmail.com>
2020-08-18 13:46:07 -0400

    Latest try

--- homework.js
+++ homework.js
@@ -17,53 +17,42 @@ function Hello(name) {
 // return true if it is odd and false if it is not. An odd number is a
 // number which, when divided by 2, has a remainder of 1 or -1.
 
-function isOdd () 
-{
-    if (num % 2 !== 0)
-    {
-    console.log('True')
-    {
-    else 
-    {
-    console.log('False')
+function isOdd(num) {
+    if (num % 2 !== 0) {
+    return ('True') }
+    else {
+    return ('False')
     }
 }
-
 
 // 5. Create a function called isEven that, given a number, will
 // return true if it is even and false if it is not. An even number is a
 // number which, when divided by 2, has a remainder of 0.
 
-function isEven () 
-{
-    if (num % 2 === 0)
-    {
-    console.log('True')
-    {
-    else 
-    {
-    console.log('False')
+function isEven(num) {
+    if (num % 2 === 0) {
+    return ('True')
+    }
+    else {
+    return ('False')
     }
 }
-
 
 // 6. Create a function called fahrenheitToCelsius that takes a
 // Fahrenheit temperature as an argument and returns the
 // temperature in Celsius. (32°F − 32) × 5/9 = 0°C
 
 function fahrenheitToCelsius(Fahrenheit) {
-    var Fahrenheit;
-    var ftoCel = Fahrenheit - 32) * 5/9;
-    var message = Fahrenheit+'\xB0F' is ' + fToCel + '\xB0C.';
-    console.log(message);
+    return (Fahrenheit - 32)*5/9
 }
-
 
 // 6. Create a function called celsiusToFahrenheit that takes a
 // Celsius temperature as an argument and returns the
 // temperature in Fahrenheit. (0°C × 9/5) + 32 = 32°F
 
-
+function celsiusToFahrenheit(Celsius) {
+    return Celsius * 9/5 + 32
+}
 
 // 7. Create a function called fahrenheitToKelvin that takes a
 // Fahrenheit temperature as an argument and returns the
@@ -72,22 +61,19 @@ function fahrenheitToCelsius(Fahrenheit) {
 // Absolute zero (0 K) is equivalent to −273.15 C.
 // 1 degree Kelvin equals 1 degree Celsius. (32°F − 32) × 5/9 + 273.15 = 273.15K
 
-
-
+function fahrenheitToKelvin (Fahrenheit) {
+    return (Fahrenheit - 32) * 5/9 + 273.15
+}
 
 // 8. Create a function called lesser that takes two numbers as
 // arguments and returns the lesser of them. This function should
 // use an if/else statement.
 
-function Lesser (num1) (num2) 
-{
-    if (num1 > num2)
-    {
-    console.log(num2)
-    {
-    else 
-    {
-    console.log(num1)
+function Lesser (num1), (num2) {
+    if (num1 > num2) {
+    return num2
+    { else {
+    return num1
     }
 }
 
